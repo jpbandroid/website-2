2c923347cc9780569682a9f20e28bff9e4709a2d
jpb <jakub.baranski.33@gmail.com>
2024-02-23 07:18:06 +0000

    Fluentize Sverdle

--- src/routes/fluent-sverdle/sverdle/how-to-play/+page.svelte
+++ src/routes/fluent-sverdle/sverdle/how-to-play/+page.svelte
@@ -4,42 +4,59 @@
 </svelte:head>
 
 <div class="text-column">
-	<h1>How to play Sverdle</h1>
+	<h1>
+		<Fluent.TextBlock variant="titleLarge">How to play Sverdle</Fluent.TextBlock>
+		
+	</h1>
 
 	<p>
-		Sverdle is a clone of <a href="https://www.nytimes.com/games/wordle/index.html">Wordle</a>, the
-		word guessing game. To play, enter a five-letter English word. For example:
+		<Fluent.TextBlock variant="body">
+			Sverdle is a clone of <a href="https://www.nytimes.com/games/wordle/index.html">Wordle</a>, the
+			word guessing game. To play, enter a five-letter English word. For example:
+		</Fluent.TextBlock>
 	</p>
 
 	<div class="example">
-		<span class="close">r</span>
-		<span class="missing">i</span>
-		<span class="close">t</span>
-		<span class="missing">z</span>
-		<span class="exact">y</span>
+		<Fluent.TextBlock variant="body">
+			<span class="close">r</span>
+			<span class="missing">i</span>
+			<span class="close">t</span>
+			<span class="missing">z</span>
+			<span class="exact">y</span>
+		</Fluent.TextBlock>
 	</div>
 
 	<p>
-		The <span class="exact">y</span> is in the right place. <span class="close">r</span> and
-		<span class="close">t</span>
-		are the right letters, but in the wrong place. The other letters are wrong, and can be discarded.
-		Let's make another guess:
+		<Fluent.TextBlock variant="body">
+			The <span class="exact">y</span> is in the right place. <span class="close">r</span> and
+			<span class="close">t</span>
+			are the right letters, but in the wrong place. The other letters are wrong, and can be discarded.
+			Let's make another guess:
+		</Fluent.TextBlock>
 	</p>
 
 	<div class="example">
-		<span class="exact">p</span>
-		<span class="exact">a</span>
-		<span class="exact">r</span>
-		<span class="exact">t</span>
-		<span class="exact">y</span>
+		<Fluent.TextBlock variant="body">
+			<span class="exact">p</span>
+			<span class="exact">a</span>
+			<span class="exact">r</span>
+			<span class="exact">t</span>
+			<span class="exact">y</span>
+		</Fluent.TextBlock>
 	</div>
 
-	<p>This time we guessed right! You have <strong>six</strong> guesses to get the word.</p>
+	<p>
+		<Fluent.TextBlock variant="body">
+			This time we guessed right! You have <strong>six</strong> guesses to get the word.
+		</Fluent.TextBlock>
+			</p>
 
 	<p>
-		Unlike the original Wordle, Sverdle runs on the server instead of in the browser, making it
-		impossible to cheat. It uses <code>&lt;form&gt;</code> and cookies to submit data, meaning you can
-		even play with JavaScript disabled!
+		<Fluent.TextBlock variant="body">
+			Unlike the original Wordle, Sverdle runs on the server instead of in the browser, making it
+			impossible to cheat. It uses <code>&lt;form&gt;</code> and cookies to submit data, meaning you can
+			even play with JavaScript disabled!
+		</Fluent.TextBlock>
 	</p>
 </div>
 
